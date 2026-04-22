f4e17338b9337ed90bb8300d1f92bfc049fdd03f
Aneel Nair <aneel@example.com>
2026-04-22 10:39:58 +0000

    updated

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const http = require("http");
 
-http.createServer((req, res) => {
-  res.end("Hello from Jenkins CI/CD 🚀");
-}).listen(3000);const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Jenkins CI/CD 🚀\n");
+});
 
-http.createServer((req, res) => {
-  res.end("Hello from Jenkins CI/CD 🚀");
-}).listen(3000);
+server.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
